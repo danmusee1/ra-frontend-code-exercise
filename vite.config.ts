1e1b5885a28f6f6cdff0befa86a7e780cfd914c8
danmusee1 <dmusee721@gmail.com>
2026-06-14 12:34:49 +0300

    configuration of tanstackrouter with autocode splitting in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
-  plugins: [svgr(), react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    svgr(), 
+    react(), 
+    tailwindcss()
+  ],
   resolve: { alias: { "@": "/src" } },
   test: {
     environment: "jsdom",
